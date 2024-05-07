f210f1bfd9062f254e8674a11764f55e785f27d1
0xJoeMama <0xjoemama@gmail.com>
2024-05-07 12:15:11 +0300

    chore: LICENSE

--- testmod/src/main/java/io/github/joemama/testmod/Testmod.java
+++ testmod/src/main/java/io/github/joemama/testmod/Testmod.java
@@ -1,7 +1,6 @@
 package io.github.joemama.testmod;
 
 import felis.kittens.core.CommonEntrypoint;
-import felis.kittens.core.event.GameEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -13,8 +12,6 @@ import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Objects;
 
 public class Testmod implements CommonEntrypoint {
     public static final Item TEST_ITEM = new Item(new Item.Properties());
@@ -28,6 +25,6 @@ public class Testmod implements CommonEntrypoint {
         // TODO: Once registry sync is added this need to be removed, furthermore, this technically doesn't work on servers
         AW_TEST.getStateDefinition().getPossibleStates().forEach(Block.BLOCK_STATE_REGISTRY::add);
         Registry.register(BuiltInRegistries.ITEM, new ResourceLocation("testmod", "aw_test"), new BlockItem(AW_TEST, new Item.Properties()));
-        GameEvents.Player.tick.end.register(player -> LOGGER.info(Objects.requireNonNull(player.getDisplayName()).getString()));
+        // GameEvents.Player.tick.end.register(player -> LOGGER.info(Objects.requireNonNull(player.getDisplayName()).getString()));
     }
 }
