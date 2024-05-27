8f0d83c934d64cdc0681923e451a20dbb421a6d5
0xJoeMama <0xjoemama@gmail.com>
2024-05-27 17:41:22 +0300

    chore: some lang testing

--- testmod/src/main/java/io/github/joemama/testmod/TestmodClient.java
+++ testmod/src/main/java/io/github/joemama/testmod/TestmodClient.java
@@ -5,15 +5,18 @@ import felis.kittens.core.client.ClientEntrypoint;
 import felis.kittens.event.GameEvents;
 import felis.kittens.keymapping.KeyMappingRegistry;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 public class TestmodClient implements ClientEntrypoint {
-    public static final KeyMapping MY_MAPPING = new KeyMapping("Stuff and things", InputConstants.Type.KEYSYM, InputConstants.KEY_U, "category1");
+    public static final KeyMapping MY_MAPPING = new KeyMapping("test.key.mapping", InputConstants.Type.KEYSYM, InputConstants.KEY_U, "category1");
 
     @Override
     public void onClientInit() {
         KeyMappingRegistry.register(MY_MAPPING);
         GameEvents.Client.Render.gui.register(ctx -> {
-            if (MY_MAPPING.consumeClick()) Testmod.LOGGER.info("Stuff happened");
+            if (MY_MAPPING.isDown()) Testmod.LOGGER.info("Stuff happened");
+            ctx.getGfx().renderItem(new ItemStack(Items.COBBLED_DEEPSLATE), 0, 0);
         });
         Testmod.LOGGER.info("Initialized");
     }
